6e366b19d4061872c9d81e8048172f15b70a608f
Fedefirewall <61513937+Fedefirewall@users.noreply.github.com>
2022-12-16 17:41:40 +0100

    login errors

--- web2/users/views.py
+++ web2/users/views.py
@@ -12,18 +12,21 @@ def home(request):
             signup_form = SignupForm()
             login_form = LoginForm(request.POST, data=request.POST)
             context = {'login_form': login_form, 'signup_form':signup_form}
+            print("login form is valid?")
+            print(login_form.is_valid())
             if login_form.is_valid():
                 username = login_form.cleaned_data.get('username')
                 password = login_form.cleaned_data.get('password')
                 user = authenticate(username=username, password=password)
+                print("useris none?")
+                print(user is None)
                 if user is not None:
                     login(request, user)
                     messages.info(request, f"You are now logged in as {username}.")
-                    return render(request, 'mango.html', context)
-                else:
-                    messages.error(request,"Invalid username or password.")
-
-            return render(request, 'test.html', context)
+                    return render(request, 'test.html', context)
+            else:
+                messages.error(request,"Invalid username or password.")
+                return render(request, 'login.html', context)
 
         elif 'signup' in request.POST:
             login_form = LoginForm()
@@ -32,10 +35,10 @@ def home(request):
             if signup_form.is_valid():
                 signup_form.save()
                 messages.success(request, f'Your account has been created. You can log in now!')    
-                return redirect('succesfully login')
             else:
                 messages.warning(request, f'Invalid input. Please try again.')
-                return render(request, 'login.html', context)
+                
+            return render(request, 'login.html', context)
     else:
         login_form = LoginForm()
         signup_form = SignupForm()
