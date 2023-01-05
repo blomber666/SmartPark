56f9cc9a7942a4ed7f798dbb3681c8506c1b2d7f
Fedefirewall <61513937+Fedefirewall@users.noreply.github.com>
2023-01-05 18:03:23 +0100

    tailwind start

--- web2/theme/static_src/tailwind.config.js
+++ web2/theme/static_src/tailwind.config.js
@@ -7,7 +7,7 @@
 
 module.exports = {
     content: [
-        /*
+        /**
          * HTML. Paths to Django template files that will contain Tailwind CSS classes.
          */
 
@@ -50,41 +50,9 @@ module.exports = {
          * for forms. If you don't like it or have own styling for forms,
          * comment the line below to disable '@tailwindcss/forms'.
          */
-        
         require('@tailwindcss/forms'),
         require('@tailwindcss/typography'),
         require('@tailwindcss/line-clamp'),
         require('@tailwindcss/aspect-ratio'),
     ],
 }
-
-
-module.exports = {
-    content: ['.//**/*.{html,js}'],
-    theme: {
-      colors: {
-        'blue': '#1fb6ff',
-        'purple': '#7e5bef',
-        'pink': '#ff49db',
-        'orange': '#ff7849',
-        'green': '#13ce66',
-        'yellow': '#ffc82c',
-        'gray-dark': '#273444',
-        'gray': '#8492a6',
-        'gray-light': '#d3dce6',
-      },
-      fontFamily: {
-        sans: ['Graphik', 'sans-serif'],
-        serif: ['Merriweather', 'serif'],
-      },
-      extend: {
-        spacing: {
-          '8xl': '96rem',
-          '9xl': '128rem',
-        },
-        borderRadius: {
-          '4xl': '2rem',
-        }
-      }
-    },
-  }
