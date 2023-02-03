aa2972e13b43d25f039182b73b22e7c61fbb6964
blomber666 <84716352+blomber666@users.noreply.github.com>
2023-02-03 12:55:42 +0100

    pagina administration completata

--- web_app/static/js/change_buttons.js
+++ web_app/static/js/change_buttons.js
@@ -1,24 +1,22 @@
 function edit_click(price_id) {
 
-    var edit = document.getElementById('edit-'+ price_id);
-    var remove = document.getElementById('remove-'+ price_id);
-    var confirm = document.getElementById('confirm-'+ price_id);
-    var cancel = document.getElementById('cancel-'+ price_id);
+  var edit = document.getElementById('edit-'+ price_id);
+  var remove = document.getElementById('remove-'+ price_id);
+  var confirm = document.getElementById('confirm-'+ price_id);
+  var cancel = document.getElementById('cancel-'+ price_id);
 
-    edit.style.display = 'none';
-    remove.style.display = 'none';
-    confirm.style.display = 'block';
-    cancel.style.display = 'block';
-    confirm.setAttribute("name", "edit");
+  edit.style.display = 'none';
+  remove.style.display = 'none';
+  confirm.style.display = 'block';
+  cancel.style.display = 'block';
+  confirm.setAttribute("name", "edit");
 
-    var keys = ["date-", "day-", "s_time-", "e_time-", "price-"];
-    keys.forEach(function(key) {
-        const elem = document.getElementById(key + price_id);
-        if (elem) {
-          elem.setAttribute("contenteditable", "true");
-          elem.style.backgroundColor = "white";
-        }
-      });
+  var keys = ["date", "day", "s_time", "e_time", "price"];
+  keys.forEach(function(key) {
+      const elem = document.getElementById(key +'-'+ price_id);
+      elem.innerHTML = '<input scope="row" id="inp_'+key+'-'+price_id+'" name="price_'+key+'" form="price_form-'+price_id+'" style="padding : 0.5rem; width : 70%;" value="'+elem.innerText+'">';
+    }
+  );
 };
 
 function delete_click(price_id) {
@@ -36,15 +34,13 @@ function delete_click(price_id) {
 };
 
 function cancel_click(price_id) {
-
-    var keys = ["date-", "day-", "s_time-", "e_time-", "price-"];
+    var keys = ["date", "day", "s_time", "e_time", "price"];
     keys.forEach(function(key) {
-        const elem = document.getElementById(key + price_id);
-        if (elem) {
-          elem.setAttribute("contenteditable", "false");
-          elem.style.backgroundColor = "";
-        }
-      });
+        const inp = document.getElementById('inp_'+ key +'-'+ price_id);
+        const td = document.getElementById(key +'-'+ price_id);
+        td.innerHTML = inp.getAttribute("value");
+        // inp.remove();
+    });
 
     var edit = document.getElementById('edit-'+ price_id);
     var remove = document.getElementById('remove-'+ price_id);
