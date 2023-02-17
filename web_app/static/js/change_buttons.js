899ebcbfc763b8174aaf75a4e7ccb218fa27b8fe
blomber666 <84716352+blomber666@users.noreply.github.com>
2023-02-17 01:19:01 +0100

    corretto bug bottone cancel

--- web_app/static/js/change_buttons.js
+++ web_app/static/js/change_buttons.js
@@ -13,22 +13,19 @@ function edit_click(price_id) {
 
     var keys = ["date-", "day-", "s_time-", "e_time-", "price-"];
     keys.forEach(function(key) {
-        const elem = document.getElementById(key + price_id);
+        var elem = document.getElementById(key + price_id);
         if (elem) {
           // create an input element and remove the elem from the DOM, but mantain its content, id and class
-          const input = document.createElement("input");
-          var value = elem.innerHTML;
-          //remove all spaces
-          value = value.replace(/\s/g, '');
-          input.value = value;
+          var input = document.createElement("input");
+          input.setAttribute("scope", "row");
           input.id = elem.id;
-          input.className = elem.className;
-          //set input form = price_form-{{price.id}}
+          input.setAttribute("placeholder", elem.innerHTML)
+          input.value = elem.innerHTML;
           input.setAttribute("form", "price_form-" + price_id);
+          input.setAttribute("class", "val-chart")
           elem.parentNode.replaceChild(input, elem);
-
-          // set the input as editable and change its background color
           input.style.backgroundColor = "#f2f2f2";
+
           if (key == "price-") {
             input.setAttribute("type", "number");
             input.setAttribute("step", "0.01");
@@ -45,16 +42,14 @@ function edit_click(price_id) {
           if (key == "s_time-") {
             input.setAttribute("type", "time");
             input.name = "price_start_time";
-            //remove the seconds from the time
             input.value = input.value.substring(0, 5);
           }
           if (key == "e_time-") {
             input.setAttribute("type", "time");
             input.name = "price_end_time";
-            //remove the seconds from the time
             input.value = input.value.substring(0, 5);
           }          
-        }
+         }
       });
 };
 
@@ -73,12 +68,18 @@ function delete_click(price_id) {
 };
 
 function cancel_click(price_id) {
-    var keys = ["date", "day", "s_time", "e_time", "price"];
+    var keys = ["date-", "day-", "s_time-", "e_time-", "price-"];
     keys.forEach(function(key) {
-        const inp = document.getElementById('inp_'+ key +'-'+ price_id);
-        const td = document.getElementById(key +'-'+ price_id);
-        td.innerHTML = inp.getAttribute("value");
-        // inp.remove();
+        var input = document.getElementById(key + price_id);
+        if (input) {
+          var td = document.createElement("td");
+          td.setAttribute("scope","row");
+          td.id = input.id;
+          td.setAttribute("class","val-chart");
+          td.innerHTML = input.getAttribute("placeholder");
+          input.parentNode.replaceChild(td, input);
+          input.remove();
+        }
     });
 
     var edit = document.getElementById('edit-'+ price_id);
